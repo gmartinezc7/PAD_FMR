7006b683d662b20acc476f08bf6d424f8d8b6df5
Roi <r.lopezbarata@gmail.com>
2024-10-28 18:59:06 +0100

    Created the list of options and starting to implement firebase database.

--- app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/config/ConfigFragment.java
+++ app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/config/ConfigFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import es.ucm.fdi.v3findmyroommate.databinding.FragmentConfigBinding;
 
@@ -26,6 +30,18 @@ public class ConfigFragment extends Fragment {
 
         final TextView textView = binding.textConfig;
         configViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        List<ConfigListItem> items = new ArrayList<>();
+        items.add(new ConfigListItem("Nombre de usuario", "Default name..."));
+        items.add(new ConfigListItem("E-mail", "Default email"));
+        items.add(new ConfigListItem("Contraseña", "Default blabla"));
+
+        ListView listView = binding.listaOpcionesConfiguracion; // Make sure this matches your ListView ID
+
+        ConfigListAdapter adapter = new ConfigListAdapter(getContext(), items);
+        listView.setAdapter(adapter);
+
+
         return root;
     }
 
