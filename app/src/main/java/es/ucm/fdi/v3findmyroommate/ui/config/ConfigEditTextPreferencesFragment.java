7634e53689c9462d0a1c0924de800047d59c3aed
Roi <r.lopezbarata@gmail.com>
2024-11-20 14:54:54 +0100

    Tried to add modify preferences button.

--- app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/config/ConfigEditTextPreferencesFragment.java
+++ app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/config/ConfigEditTextPreferencesFragment.java
@@ -13,16 +13,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
--- app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/config/ConfigFragment.java
+++ app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/config/ConfigFragment.java
@@ -12,12 +12,22 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import es.ucm.fdi.v3findmyroommate.Lobby;
 import es.ucm.fdi.v3findmyroommate.MainActivity;
+import es.ucm.fdi.v3findmyroommate.PreferencesFragment.BaseFragment;
+import es.ucm.fdi.v3findmyroommate.PreferencesFragment.PersonalInformationFragment;
+import es.ucm.fdi.v3findmyroommate.PreferencesFragment.PreferenceUser;
+import es.ucm.fdi.v3findmyroommate.PreferencesFragment.PropertyTypeFragment;
 import es.ucm.fdi.v3findmyroommate.R;
+import es.ucm.fdi.v3findmyroommate.SignUp;
 import es.ucm.fdi.v3findmyroommate.databinding.FragmentConfigBinding;
 
 public class ConfigFragment extends Fragment {
@@ -53,9 +63,112 @@ public class ConfigFragment extends Fragment {
                 Toast logoutToast = Toast.makeText(getContext(), R.string.logout_toast_text,
                         Toast.LENGTH_SHORT);
                 logoutToast.show();
-                Log.e("Logout", "Logout successful");
+                Log.d("Logout", "Logout successful");
             }
             else throw new NullPointerException("Error finishing activity");
+        });
+
+
+        Button mofifyPreferencesButton = root.findViewById(R.id.modify_preferences_button);
+        mofifyPreferencesButton.setOnClickListener(view -> {
+            Intent intent = new Intent(ConfigFragment. this,PreferenceUser.class);
+            //Intent intent = new Intent(getContext(), PropertyTypeFragment.class);
+            //Fragment currentFragment = getActivity().getSupportFragmentManager().findFragmentById(R.id.preferences_fragment);
+
+            //NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+            //NavigationUI.setupActionBarWithNavController(this.getActivity(), navController);
+            //navController.navigate(R.id.propertyTypeFragment);
+            getChildFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.preferences_fragment, new PropertyTypeFragment())
+                    .commit();
+
+
+
+            /*
+            Fragment newFragment = new PropertyTypeFragment(); // Replace with your target fragment
+            //FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+
+            Fragment currentFragment = requireActivity().getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+            if (currentFragment != null) {
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.remove(currentFragment);
+                transaction.commitNow(); // Commit immediately to ensure cleanup
+                transaction.show(newFragment);
+                transaction.commitNow(); // Commit immediately to ensure cleanup
+            }
+
+             */
+            /*
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null); // Optional: Add to back stack for navigation
+            transaction.commit();
+
+             */
+
+
+            if (getActivity() != null) {
+                    /*
+
+                FragmentActivity activity = getActivity(); // Obtén la actividad actual
+                if (activity != null) {
+                    FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+
+                    // Reemplaza el fragmento padre (ConfigFragment) por PropertyTypeFragment
+                    transaction.replace(R.id.fragment_container, new PropertyTypeFragment());
+
+                    // Opcional: Añadir a la pila de retroceso
+                    transaction.addToBackStack(null);
+
+                    // Finaliza la transacción
+                    transaction.commit();
+                    Log.d("ModifyPreferences", "Parent fragment replaced successfully");
+                }
+                if (isAdded()) {
+                    FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+                    Fragment currentFragment = getChildFragmentManager().findFragmentById(R.id.preferences_frame);
+
+                    if (currentFragment != null) {
+                        transaction.remove(currentFragment);
+                    }
+
+                    transaction.replace(R.id.preferences_frame, new PropertyTypeFragment());
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                }
+
+                     */
+
+                /*
+
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Fragment currentFragment = getActivity().getSupportFragmentManager().findFragmentById(R.id.preferences_fragment);
+                if (currentFragment != null) {
+                    transaction.detach(currentFragment);
+                }
+                transaction.replace(R.id.preferences_fragment, new PropertyTypeFragment());
+                transaction.addToBackStack(null);
+                transaction.setReorderingAllowed(true);
+                transaction.commit();
+                 */
+
+            }
+
+
+
+
+            //Intent intent = new Intent(getContext().getApplicationContext(), Lobby.class);
+            //startActivity(intent);
+            /*
+            FragmentActivity currentActivity = getActivity();
+            if (currentActivity != null) {
+                //currentActivity.finish();
+                Log.d("ModifyPreferences", "Successfully initialized the modification fragment");
+            }
+            else throw new NullPointerException("Error loading the preference's modification fragment");
+
+             */
         });
 
         return root;
