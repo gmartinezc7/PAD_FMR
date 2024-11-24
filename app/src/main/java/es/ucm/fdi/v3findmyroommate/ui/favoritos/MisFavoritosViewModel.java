5bcb67fd87d63605a3e4f37608927d58bc2db7c1
gmartinezcarrera <gonzalomartinezc98@gmail.com>
2024-11-24 16:39:31 +0100

    añadido favoritos falta condicion

--- app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/favoritos/MisFavoritosViewModel.java
+++ app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/favoritos/MisFavoritosViewModel.java
@@ -1,19 +1,53 @@
 package es.ucm.fdi.v3findmyroommate.ui.favoritos;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import es.ucm.fdi.v3findmyroommate.ui.home.Vivienda;
+
+
 public class MisFavoritosViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private MutableLiveData<List<Vivienda>> viviendasfavs;
 
     public MisFavoritosViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is mis favoritos fragment");
+        viviendasfavs = new MutableLiveData<>();
+        cargarViviendasFavs();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Vivienda>> getViviendas () { return viviendasfavs; }
+
+    private void cargarViviendasFavs (){
+        FirebaseDatabase databaser = FirebaseDatabase.getInstance("https://findmyroommate-86cbe-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaser.getReference("viviendas").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<Vivienda> lista = new ArrayList<>();
+                for (DataSnapshot viviendas : snapshot.getChildren()){
+                    Vivienda vivienda = viviendas.getValue(Vivienda.class);
+                    if (vivienda != null) {
+                        lista.add(vivienda);
+                        System.out.println("Vivienda carga: " + vivienda.getTitle());
+                    }
+                }
+                viviendasfavs.setValue(lista);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Error en DATABASE");
+
+            }
+        });
+
     }
 }
