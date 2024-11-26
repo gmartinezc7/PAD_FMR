3df01812cc9db2c22c5ab6b473868bc8db8928bb
gmartinezcarrera <gonzalomartinezc98@gmail.com>
2024-11-26 21:24:43 +0100

    CAMBIOS DE FAVORITOS OK
    Ahora los favoritos funcionan con los objetos de add

--- app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/home/ViviendaAdapter.java
+++ app/src/main/java/es/ucm/fdi/v3findmyroommate/ui/home/ViviendaAdapter.java
@@ -1,6 +1,8 @@
 package es.ucm.fdi.v3findmyroommate.ui.home;
 
+import com.google.android.material.chip.Chip;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.List;
 
@@ -47,9 +50,23 @@ public class ViviendaAdapter extends RecyclerView.Adapter<ViviendaAdapter.Vivien
         holder.name.setText(vivienda.getTitle());
         holder.address.setText(vivienda.getLocation());
         holder.description.setText(vivienda.getDescription());
-        holder.price.setText(vivienda.getPrice().toString());
-        holder.metr.setText(vivienda.getMetr().toString());
+        holder.price.setText(vivienda.getPrice());
+        holder.metr.setText(vivienda.getMetr());
 
+        // El resto de atributos, como son chips que quiero mostrar y no mostrar dependiendo de la
+        // categoría, pues los cargamos dependiendo de si son atributos de esa categoría o no, con
+        // el siguiente método
+
+        setChipVisibility(holder.categoria, vivienda.getCategoria(),"Categoria - ");
+        setChipVisibility(holder.tipoCasa, vivienda.getTipoCasa(), "Tipo Casa - ");
+        setChipVisibility(holder.habitaciones, vivienda.getHabitaciones(), "Habitaciones - ");
+        setChipVisibility(holder.banos, vivienda.getBanos(), "Baños - ");
+        setChipVisibility(holder.exteriorInterior, vivienda.getExteriorInterior(), "Orientacion - ");
+        setChipVisibility(holder.companeros, vivienda.getCompaneros(), "Compañeros - ");
+        setChipVisibility(holder.genero, vivienda.getGenero(), "Género - ");
+        setChipVisibility(holder.tipoBano, vivienda.getTipoBano(), "Tipo Baño - ");
+
+        String key = vivienda.getId();
         String vivKey = database.getReference("viviendas").child(String.valueOf(position)).getKey();
         System.out.println("VALOR DE KEY: " + vivKey);
 
@@ -66,20 +83,7 @@ public class ViviendaAdapter extends RecyclerView.Adapter<ViviendaAdapter.Vivien
 
                     holder.toggleFavorito.setOnCheckedChangeListener((buttonView, isChecked) -> {
                         if (isChecked){// Agregar favoritos
-                            // Arreglo para poder pasar ids de las viviendas
-                            int num = Integer.parseInt(vivKey);
-                            num++;
-                            // Los siguientes IFS son para controlar la construcción de los ids (entendemos que no va a haber más de 1000 viviendas)
-                            Object newKey ="";
-                            if (num < 10){
-                                newKey = "v00" + num;
-                            }else if(num >= 10 && num < 100){
-                                newKey = "v0" + num;
-                            }else if(num >= 100 && num < 1000){
-                                newKey = "v" + num;
-                            }
-                            database.getReference("users").child(userId).child("listfavoritos").child(vivKey).setValue(newKey);
-
+                            database.getReference("users").child(userId).child("listfavoritos").child(vivKey).setValue(key);
                         }else{// Quitar de favs
                             database.getReference("users").child(userId).child("listfavoritos").child(vivKey).removeValue();
                         }
@@ -96,7 +100,6 @@ public class ViviendaAdapter extends RecyclerView.Adapter<ViviendaAdapter.Vivien
         }
 
 
-
     }
 
     @Override
@@ -108,6 +111,7 @@ public class ViviendaAdapter extends RecyclerView.Adapter<ViviendaAdapter.Vivien
         private TextView name, address, description, price;
         private TextView metr;
         private ToggleButton toggleFavorito;
+        private Chip categoria, tipoCasa, habitaciones, banos, exteriorInterior, companeros, genero, tipoBano;
 
         public ViviendaViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -117,10 +121,29 @@ public class ViviendaAdapter extends RecyclerView.Adapter<ViviendaAdapter.Vivien
             description = itemView.findViewById(R.id.description);
             metr = itemView.findViewById(R.id.metr);
             toggleFavorito = itemView.findViewById(R.id.toggleButton);
+            categoria = itemView.findViewById(R.id.chipCategoria);
+            tipoCasa = itemView.findViewById(R.id.chipTipoCasa);
+            habitaciones = itemView.findViewById(R.id.chipHabitaciones);
+            banos = itemView.findViewById(R.id.chipBanos);
+            exteriorInterior = itemView.findViewById(R.id.chipExtInt);
+            companeros = itemView.findViewById(R.id.chipCompaneros);
+            genero = itemView.findViewById(R.id.chipGenero);
+            tipoBano = itemView.findViewById(R.id.chipTipoBano);
         }
     }
     public void updateList (List<Vivienda> newVivs){
         this.listViv = newVivs;
         notifyDataSetChanged();
+    }
+
+    // Método auxiliar para cargar vista
+    private void setChipVisibility (com.google.android.material.chip.Chip chip, String value, String prefix){
+        if (value == null || value.trim().isEmpty()){
+            chip.setVisibility(View.GONE);
+        }else{
+            chip.setVisibility(View.VISIBLE);
+            chip.setText(prefix + value);
+        }
+
     }
 }
