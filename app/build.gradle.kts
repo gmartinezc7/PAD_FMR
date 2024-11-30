6a9060763fc30f3ebed837cf2f2fed373846e3d4
jjencina <91247078+jjencina@users.noreply.github.com>
2024-11-30 20:54:18 +0100

    Añadir FirebaseMessagingService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.firebase.storage)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.inappmessaging.display)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
